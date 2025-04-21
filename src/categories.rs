bef44b780783e07b0ce25dea3ad72ca97d636d05
Lasne Olivier <lasneolivier@gmail.com>
2025-04-21 18:44:18 +0200

    not sure, but it seems that's how you build a factory

--- src/categories.rs
+++ src/categories.rs
@@ -12,7 +12,7 @@ pub struct EmojiCollection {
     grid_view_wrapper: TypedGridView<Emoji, gtk::SingleSelection>,
 }
 
-#[relm4::factory]
+#[relm4::factory(pub)]
 impl FactoryComponent for EmojiCollection {
     type Init = (String, Vec<Emoji>);
     type Input = Msg;
@@ -20,14 +20,17 @@ impl FactoryComponent for EmojiCollection {
     type CommandOutput = ();
     type ParentWidget = gtk::Box;
 
-    fn init_model(init: Self::Init) -> Self {
+    fn init_model(init: Self::Init, _index: &DynamicIndex, _sender: FactorySender<Self>) -> Self {
+
         let (category, emojis) = init;
+
         let mut grid_view_wrapper: TypedGridView<Emoji, gtk::SingleSelection> = TypedGridView::new();
         grid_view_wrapper.add_filter(|item| item.name == "smile");
         grid_view_wrapper.set_filter_status(0, false);
         for emoji in emojis {
             grid_view_wrapper.append(Emoji::new(&emoji.symbol, &emoji.name));
         }
+        
         Self {
             category,
             grid_view_wrapper,
@@ -44,51 +47,50 @@ impl FactoryComponent for EmojiCollection {
                 set_label: "Hi mom!"
             },
 
-            #[local_ref]
-            my_view -> gtk::GridView {
+            gtk::GridView {
                 set_orientation: gtk::Orientation::Vertical,
                 set_max_columns: 10,
             }
         }
     }
 
-    fn init(
-        emoji_collection: Self::Init,
-        root: Self::Root,
-        _sender: ComponentSender<Self>,
-    ) -> ComponentParts<Self> {
+    // fn init(
+    //     emoji_collection: Self::Init,
+    //     root: Self::Root,
+    //     _sender: ComponentSender<Self>,
+    // ) -> ComponentParts<Self> {
         
-        // Initialize the GridView wrapper
-        let mut grid_view_wrapper: TypedGridView<Emoji, gtk::SingleSelection> =
-            TypedGridView::new();
+    //     // Initialize the GridView wrapper
+    //     let mut grid_view_wrapper: TypedGridView<Emoji, gtk::SingleSelection> =
+    //         TypedGridView::new();
 
-        // Add a filter and disable it
-        grid_view_wrapper.add_filter(|item| item.name == "smile");
-        grid_view_wrapper.set_filter_status(0, false);
+    //     // Add a filter and disable it
+    //     grid_view_wrapper.add_filter(|item| item.name == "smile");
+    //     grid_view_wrapper.set_filter_status(0, false);
 
-        let (category, emojis) = emoji_collection;
+    //     let (category, emojis) = emoji_collection;
 
-        // Add the emojis to the GridView
-        for emoji in emojis {
-            grid_view_wrapper.append(
-                Emoji::new(&emoji.symbol, &emoji.name)
-            );
-        }
+    //     // Add the emojis to the GridView
+    //     for emoji in emojis {
+    //         grid_view_wrapper.append(
+    //             Emoji::new(&emoji.symbol, &emoji.name)
+    //         );
+    //     }
         
-        let model = EmojiCollection {
-            category,
-            grid_view_wrapper,
-        };
+    //     let model = EmojiCollection {
+    //         category,
+    //         grid_view_wrapper,
+    //     };
 
-        let my_view = &model.grid_view_wrapper.view;
+    //     let my_view = &model.grid_view_wrapper.view;
 
-        let widgets = view_output!();
+    //     let widgets = view_output!();
 
-        ComponentParts { model, widgets }
-    }
+    //     ComponentParts { model, widgets }
+    // }
 
 
-    fn update(&mut self, msg: Self::Input, _sender: ComponentSender<Self>) {
+    fn update(&mut self, msg: Self::Input, _sender: FactorySender<Self>) {
         match msg {
             Msg::OnlyShowEven(show_only_even) => {
                 // Disable or enable the first filter
