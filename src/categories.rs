6b04ad2a01160bece4e11e86227a7de2bb9cb59f
Lasne Olivier <lasneolivier@gmail.com>
2025-04-21 18:44:50 +0200

    cleanup

--- src/categories.rs
+++ src/categories.rs
@@ -54,42 +54,6 @@ impl FactoryComponent for EmojiCollection {
         }
     }
 
-    // fn init(
-    //     emoji_collection: Self::Init,
-    //     root: Self::Root,
-    //     _sender: ComponentSender<Self>,
-    // ) -> ComponentParts<Self> {
-        
-    //     // Initialize the GridView wrapper
-    //     let mut grid_view_wrapper: TypedGridView<Emoji, gtk::SingleSelection> =
-    //         TypedGridView::new();
-
-    //     // Add a filter and disable it
-    //     grid_view_wrapper.add_filter(|item| item.name == "smile");
-    //     grid_view_wrapper.set_filter_status(0, false);
-
-    //     let (category, emojis) = emoji_collection;
-
-    //     // Add the emojis to the GridView
-    //     for emoji in emojis {
-    //         grid_view_wrapper.append(
-    //             Emoji::new(&emoji.symbol, &emoji.name)
-    //         );
-    //     }
-        
-    //     let model = EmojiCollection {
-    //         category,
-    //         grid_view_wrapper,
-    //     };
-
-    //     let my_view = &model.grid_view_wrapper.view;
-
-    //     let widgets = view_output!();
-
-    //     ComponentParts { model, widgets }
-    // }
-
-
     fn update(&mut self, msg: Self::Input, _sender: FactorySender<Self>) {
         match msg {
             Msg::OnlyShowEven(show_only_even) => {
